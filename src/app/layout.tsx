2e1aea377d5d552f5de803ce3a9d8d84feb5f8d7
Aleffelisberto <aleffelisberto@gmail.com>
2023-02-25 05:21:46 -0300

    feat(layout): add in progress and menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
 import '../styles/global.css';
+import { JetBrains_Mono } from '@next/font/google';
+import { Menu } from '@/components/Menu';
+import Image from 'next/image';
+import matrixBg from '../assets/matrix-bg.gif';
+
+const jetBrainsMono = JetBrains_Mono({ subsets: ['latin'] });
 
 export const metadata = {
   title: {
@@ -14,8 +20,23 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={jetBrainsMono.className}>
+      <body className="h-screen bg-[#0e0e0f] overflow-hidden">
+        <div className="flex flex-row">
+          <div className="max-w-min h-screen p-5 bg-[#030303]">
+            <div className="grid place-items-center h-screen">
+              <Menu />
+            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center w-full relative">
+            <Image className="opacity-10 w-full absolute" src={matrixBg} fill alt="falling green characters as background" />
+            <div className="text-center">
+              <h1 className="lg:text-7xl md:text-6xl text-4xl text-[#66dd5e]">IN PROGRESS... :)</h1>
+            </div>
+          </div>
+        </div>
+        <div>{children}</div>
+      </body>
     </html>
   );
 }
